56fe277837aacae06129920dc36ecae1a8bd83f1
Robin Lifshitz <Rlifshitz19@gmail.com>
2023-06-16 10:53:10 -0700

    reverse strings with graphemes

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,3 +1,24 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    unimplemented!("Write a function to reverse {input}");
+    // let allchars: Vec<char> = input.chars().collect();
+    // let mut reversed_string_vec: Vec<char> = vec![];
+
+    // println!("{:?}", allchars);
+
+    // for c in allchars {
+    //     reversed_string_vec.insert(0, c);
+    // }
+
+    
+    // let reversed_string:String = reversed_string_vec.iter().collect();
+    
+    // println!("{}", reversed_string);
+
+
+
+    let reversed_string = input.graphemes(true).rev().collect();
+    println!("{}", reversed_string);
+    reversed_string
+
 }
