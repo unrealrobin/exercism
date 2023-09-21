c7cb473f3d0e8dcdcfc6af7def90900a1f32bb31
Robin Lifshitz <Rlifshitz19@gmail.com>
2023-09-20 19:30:51 -0700

    Grains, all tests passing, ez

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,7 +1,23 @@
 pub fn square(s: u32) -> u64 {
-    todo!("grains of rice on square {s}");
+
+    if s < 1 || s > 64 {
+        panic!("Square must be between 1 and 64")
+    }
+
+    let base: u64 = 2;
+    let total: u64 = base.pow(s - 1);
+    total
 }
 
-pub fn total() -> u64 {
-    todo!();
+pub fn total() -> u64 { 
+    let mut total: u64 = 0;
+    let mut index:u32 = 1;
+
+    while index <= 64 {
+        total += square(index) as u64;
+        index+=1;
+    }
+    
+
+    total
 }
