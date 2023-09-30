e7713c08688ad5524287d2e38e23e172e4b7f7d9
Robin Lifshitz <Rlifshitz19@gmail.com>
2023-09-30 11:50:09 -0700

    Anagram, All Tests Passing

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,14 +1,28 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
     
-    let mut letters = HashSet::new();
+    let mut anagrams = HashSet::new();
 
-    for each_letter in word {
-        letters.insert(each_letter);
+    pub fn sort_the_word(some_word: &str) -> String {
+        let mut chars: Vec<char> = some_word.chars().collect();
+        chars.sort();
+        let sorted_word: String = chars.into_iter().collect();
+
+        sorted_word
+
     }
-    
-    println!("{:?}", letters);
 
-    letters
+    for words in possible_anagrams{
+        if words.to_string().to_lowercase() == word.to_string().to_lowercase() {
+            continue;
+        }
+        else if sort_the_word(&words.to_lowercase()) == sort_the_word(&word.to_lowercase()) {
+            anagrams.insert(*words);
+        }
+    }
+
+    anagrams
+
+    
 }
